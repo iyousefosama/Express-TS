dc5fd12b7de5dab4861bf9e005ca2dd4d938301a
iiBlackwolf <blackwolf3466@gmail.com>
2024-06-05 03:09:08 +0300

    Added more types

--- app.js
+++ app.js
@@ -6,21 +6,52 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
-const users = [];
+let users = [];
 // GET
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
     res.send("Hello world!");
 });
-app.get('/users', (req, res) => {
-    if (users.length == 0) {
-        return res.status(404).send("User are not found.");
+app.get("/users", (req, res) => {
+    try {
+        if (users.length == 0) {
+            return res.status(404).send("❌ No users are found.");
+        }
+        res.status(200).send(users);
     }
-    return res.status(200).send(users);
+    catch (error) {
+        res.status(400).send(error);
+    }
 });
 // POST
-app.post('/users', (req, res) => {
-    console.log(req.body);
-    res.send("Posted...");
+app.post("/users", (req, res) => {
+    try {
+        const user = req.body;
+        // Checks if user already exists in the array or not.
+        const findUser = users.find((x) => x.id === user.id);
+        if (findUser) {
+            return res.status(400).send("❌ User already found!");
+        }
+        users.push(user);
+        res.status(201).send("✅ Successfully added user.");
+    }
+    catch (error) {
+        res.status(400).send(error);
+    }
+});
+// DELETE - Remove data
+app.delete("/users/:id", (req, res) => {
+    try {
+        const { id } = req.params;
+        const userIndex = users.findIndex((x) => x.id === id); // Use findIndex instead of find
+        if (userIndex === -1) {
+            return res.status(400).send("❌ User not found!");
+        }
+        users.splice(userIndex, 1);
+        res.status(201).send("✅ User deleted successfully.");
+    }
+    catch (error) {
+        res.status(400).send(error);
+    }
 });
 app.listen(3050, () => {
     console.log("App is listening!");
--- app.ts
+++ app.ts
@@ -1,50 +1,69 @@
-import express, {Request, Response} from 'express'
+import express, { Request, Response } from "express";
 
 const app = express();
 
 app.use(express.json());
 
-const users:any[] = [];
+type User = {
+  id: string;
+  name: string;
+  age: number;
+};
+
+let users: User[] = [];
 
 // GET
-app.get('/', (req: Request, res: Response) => {
-    res.send("Hello world!")
-})
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello world!");
+});
 
-app.get('/users', (req: Request, res: Response) => {
-    if(users.length == 0) {
-        return res.status(404).send("User are not found.");
+app.get("/users", (req: Request, res: Response) => {
+  try {
+    if (users.length == 0) {
+      return res.status(404).send("❌ No users are found.");
     }
 
-    return res.status(200).send(users)
-})
+    res.status(200).send(users);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
 // POST
-app.post('/users', (req: Request, res: Response) => {
-    const user = req.body;
-    
-    // Checks if user already in the array or not.
-    const findUser = users.find((x) =>  x.id === user.id)
+app.post("/users", (req: Request, res: Response) => {
+  try {
+    const user: User = req.body;
 
-    if(findUser) {
-        return res.status(400).send("User already found!")
+    // Checks if user already exists in the array or not.
+    const findUser = users.find((x) => x.id === user.id);
+
+    if (findUser) {
+      return res.status(400).send("❌ User already found!");
     }
 
-    users.push(user)
-    res.status(201).send("Posted user...");
-})
+    users.push(user);
+    res.status(201).send("✅ Successfully added user.");
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
 // DELETE - Remove data
-app.delete('/users/:id', (req: Request, res: Response) => {
+app.delete("/users/:id", (req: Request, res: Response) => {
+  try {
     const { id } = req.params;
-    const UserIndex = users.find((x) => x.id === id)
+    const userIndex: number = users.findIndex((x) => x.id === id); // Use findIndex instead of find
 
-    if(UserIndex == -1) {
-        return res.status(400).send("User not found!");
+    if (userIndex === -1) {
+      return res.status(400).send("❌ User not found!");
     }
 
-    users.splice(UserIndex, 1);
-    res.status(201).send("User deleted successfuly.")
-})
+    users.splice(userIndex, 1);
+    res.status(201).send("✅ User deleted successfully.");
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
 
 app.listen(3050, () => {
-    console.log("App is listening!")
-})
+  console.log("App is listening!");
+});
